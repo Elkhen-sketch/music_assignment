871d71a6ae8b99defbdfc28cf6d12973f9a7a4af
D24125405 Enkhtushig Legdenbold <D24125405@mytudublin.ie>
2024-12-02 15:11:54 +0000

    bla

--- music-assignment/Elkhen/d_24125405_elkhen.gd
+++ music-assignment/Elkhen/d_24125405_elkhen.gd
@@ -9,9 +9,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
 
 func _on_button_7_button_down() -> void:
 	$Button7/AudioStreamPlayer2D.play()
 
+	pass # Replace with function body.
+
+
+func _on_button_pressed() -> void:
 	pass # Replace with function body.
