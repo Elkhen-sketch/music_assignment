f366d5538970ddfc64206bf08d0c122b865ea241
d24125405@mytudublin.ie <d24125405@mytudublin.ie>
2024-12-02 23:25:43 +0000

    ui finalized

--- music-assignment/drum_looper.gd
+++ music-assignment/drum_looper.gd
@@ -1,7 +1,7 @@
 extends  Node2D
 
 func _on_button_7_button_down() -> void:
-	$"floor drum".play()
+	$harmony.play()
 	pass # Replace with function body.
 
 
@@ -23,20 +23,32 @@ func _on_button_5_button_down() -> void:
 func _on_button_9_button_down() -> void:
 	$"bass drum".play()
 	pass # Replace with function body.
+	
 
+	
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 
 	print(event.position)
 
-	var s = remap(event.position.x, 438, 602, 0, 2)
-	$"floor drum".pitch_scale = s
+	var s = remap(event.position.x, 902, 1017, 0, 2)
+	$harmony.pitch_scale = s
 	var p = get_viewport().get_mouse_position()
 
 
-	var vol = remap(event.position.y, 153, 185, 0, 1)
+	var vol = remap(event.position.y, 42, 157, 0, 1)
 	vol = linear_to_db(vol)
 	AudioServer.set_bus_volume_db(0, vol)
 	print(p)
 	print("89Hwello from the mouse")
+	pass # Replace with function body.
+
+
+func _on_button_10_button_down() -> void:
+	$floortom.play()
+	pass # Replace with function body.
+
+
+func _on_button_11_button_down() -> void:
+	$"bass drum".play()
 	pass # Replace with function body.
