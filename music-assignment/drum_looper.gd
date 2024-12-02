fad3833243cc031ba33ead39305634c414ce7d0e
d24125405@mytudublin.ie <d24125405@mytudublin.ie>
2024-12-02 17:32:37 +0000

    added volume and speed control

--- music-assignment/drum_looper.gd
+++ music-assignment/drum_looper.gd
@@ -18,3 +18,25 @@ func _on_button_8_button_down() -> void:
 func _on_button_5_button_down() -> void:
 	$"small tom".play()
 	pass # Replace with function body.
+
+
+func _on_button_9_button_down() -> void:
+	$"bass drum".play()
+	pass # Replace with function body.
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+
+	print(event.position)
+
+	var s = remap(event.position.x, 438, 602, 0, 2)
+	$"floor drum".pitch_scale = s
+	var p = get_viewport().get_mouse_position()
+
+
+	var vol = remap(event.position.y, 153, 185, 0, 1)
+	vol = linear_to_db(vol)
+	AudioServer.set_bus_volume_db(0, vol)
+	print(p)
+	print("89Hwello from the mouse")
+	pass # Replace with function body.
