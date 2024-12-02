d6be1eeec1dbdfecfebf17fd25f62281bb73aff9
d24125405@mytudublin.ie <d24125405@mytudublin.ie>
2024-12-02 16:00:26 +0000

    finished tom set up

--- music-assignment/drum_looper.gd
+++ music-assignment/drum_looper.gd
@@ -1,10 +1,20 @@
 extends  Node2D
 
 func _on_button_7_button_down() -> void:
-	$AudioStreamPlayer2D.play()
+	$"floor drum".play()
 	pass # Replace with function body.
 
 
 func _on_button_3_button_down() -> void:
 	$mediumtom.play()
+	pass # Replace with function body.
+
+
+func _on_button_8_button_down() -> void:
+	$"snare drum".play()
+	pass # Replace with function body.
+
+
+func _on_button_5_button_down() -> void:
+	$"small tom".play()
 	pass # Replace with function body.
