7440c5bd124d12981c0e0b4fad7095eb453de14d
d24125405@mytudublin.ie <d24125405@mytudublin.ie>
2024-12-02 15:29:27 +0000

    added medium tom sound

--- music-assignment/drum_looper.gd
+++ music-assignment/drum_looper.gd
@@ -3,3 +3,8 @@ extends  Node2D
 func _on_button_7_button_down() -> void:
 	$AudioStreamPlayer2D.play()
 	pass # Replace with function body.
+
+
+func _on_button_3_button_down() -> void:
+	$mediumtom.play()
+	pass # Replace with function body.
