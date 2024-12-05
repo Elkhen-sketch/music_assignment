e179d4dd873f7c9442fde28d12b9c98e8b78e434
d24125405@mytudublin.ie <d24125405@mytudublin.ie>
2024-12-05 17:41:20 +0000

    combined two precesses

--- music-assignment/drum_looper.gd
+++ music-assignment/drum_looper.gd
@@ -16,9 +16,6 @@ func _process(delta: float) -> void:
 		else:
 			$bassdrum2.play()
 	queue_redraw()
-	pass
-	
-func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Medium tom"):
 		if $mediumtom.playing:
 			$mediumtom.stop()
